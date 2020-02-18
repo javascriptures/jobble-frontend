2a7b623727f6a176e74d7ba89f16b9a2e52352d2
Michelene Chon <michelenechon@gmail.com>
2020-02-18 12:48:41 -0800

    Minor UI tweaks

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,14 +1,17 @@
-import React from 'react'
-import turkey from '../images/jobble-turkey.png'
+import React from 'react';
+import turkey from '../images/jobble-turkey.png';
 
-function Header () {
-
-    return(
-        <nav>
-            <img className="clink"src={turkey} alt="jobble turkey" />
-            <link href="https://fonts.googleapis.com/css?family=Comfortaa&display=swap" rel="stylesheet"/>
-            <h1 className="title">Jobble</h1>
-        </nav>
-    )
+function Header() {
+  return (
+    <nav>
+      <img className="clink" src={turkey} alt="jobble turkey" />
+      <link
+        href="https://fonts.googleapis.com/css?family=Comfortaa&display=swap"
+        rel="stylesheet"
+      />
+      <h1 className="title">Jobble</h1>
+      <h5 className="title">Job Posting Management for Job Seekers</h5>
+    </nav>
+  );
 }
-export default Header
+export default Header;

0253035e0230a0798038cf424348621a98066af4
ghazalmansour <ghazalmansour@gmail.com>
2020-02-19 20:21:16 -0500

    working on about component

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,10 +1,13 @@
 import React from 'react';
-
 const About = () => {
   return (
     <>
       <h1 className="aboutHeader">About Jobble</h1>
-      <p>Welcome to Jobble, your job hunting personal assistant!</p>
+      <h2> Welcome to our app, your job hunting personal assistant!</h2>
+
+      <img src="/about-image/job-hunting.jpg" alt=""/>
+
+      <p> Jobble is a job seeker app which allows you to download and curate your own personal lists of jobs you are interested in applying for.</p>
     </>
   );
 };

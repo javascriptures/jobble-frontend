1a90295f025c4e44548636c1480713f3c4cd1957
ghazalmansour <ghazalmansour@gmail.com>
2020-02-19 21:41:56 -0500

    added team lists

--- src/Components/About.js
+++ src/Components/About.js
@@ -5,9 +5,16 @@ const About = () => {
       <h1 className="aboutHeader">About Jobble</h1>
       <h2> Welcome to our app, your job hunting personal assistant!</h2>
 
-      <img src="/about-image/job-hunting.jpg" alt=""/>
+      <img className="jobs" src="/about-images/job-hunting.jpg" alt=""/>
 
       <p> Jobble is a job seeker app which allows you to download and curate your own personal lists of jobs you are interested in applying for.</p>
+      <p2>Sign up to Jobble today by signing up/in to Dashlane. And Start Hunting Your Dream Job. </p2>
+
+      <h2> Our Team </h2>
+      <img className="pic1" title="Ghazal" src="/about-images/ghazal.jpeg" alt="" width="100" height="100" />
+      <img className="pic2" title="Michelene" src="/about-images/michelene.jpeg" alt="" width="100" height="100"/>
+      <img className="pic3" title="Jordan" src="/about-images/jordan.jpeg" alt="" width="100" height="100"/>
+      <img className="pic4" title="Qusai" src="/about-images/qusai.jpeg" alt="" width="100" height="100"/>
     </>
   );
 };

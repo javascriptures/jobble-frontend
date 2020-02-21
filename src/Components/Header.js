912f76def933c3a121d164172a62f2a6cbf0d9ef
javascriptures <jtjavascripture@gmail.com>
2020-02-20 18:40:53 -0800

    Jobble Turkey

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import Logo from './Logo';
-
 function Header() {
   return (
     <header>
       <Logo />
-      <h1 className="title">Jobble</h1>
+      <div><h1 className="title">Jobble</h1>
+      <h5 className="title" id="tagline">Job Posting Management for Job Seekers</h5></div>
     </header>
   );
 }
